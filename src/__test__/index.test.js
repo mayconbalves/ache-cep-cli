a7bc0f7effdb83fecbb81720a669fb8831d99708
mayconbalves <mayconbalves1@gmail.com>
2025-12-16 20:31:55 -0300

    feat: create mock test

---
+++ src/__test__/index.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect, jest, beforeEach } from '@jest/globals';
+
+describe('ache-cep-cli', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('should return true', () => {
+    expect(true).toBe(true);
+  });
+});
